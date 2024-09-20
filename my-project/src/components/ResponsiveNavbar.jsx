8cfa244439ef0f0cf584dc621e2a1be1c39aae4d
bilalmukhtiar5 <billuedwardian08@gmail.com>
2024-09-20 09:46:50 +0500

    responsive navbar

--- my-project/src/components/ResponsiveNavbar.jsx
+++ my-project/src/components/ResponsiveNavbar.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FiMenu } from "react-icons/fi";
 import { IoClose } from "react-icons/io5";
 
 const ResponsiveNavbar = () => {
+    const [toggle, setToggle] = useState(false)
+
   return (
     <div className='bg-slate-500 p-4'>
         <div className='flex justify-between items-center max-w-[1240] border border-black mx-auto'>
                 <div className='text-white font-extrabold text-4xl'>
                     Logo
                 </div>
-                <FiMenu className='text-white text-2xl md:hidden block'/>
-                {/* <IoClose className='text-white text-2xl md:hidden'/> */}
+
+                {
+                    toggle?
+                    <IoClose onClick={()=>setToggle(!toggle)} className='text-white text-2xl md:hidden'/>
+                        
+                        :
+                        <FiMenu onClick={()=> setToggle(!toggle)} className='text-white text-2xl md:hidden block'/>
+                }
+                
 
                 <ul className='hidden md:flex justify-around gap-5'>
                     <li>
@@ -31,8 +40,9 @@ const ResponsiveNavbar = () => {
                 </ul>
 
                 {/* Responsive */}
-                <ul className='md:hidden w-full fixed bg-black left-0 top-[138px] text-white'>
-                    <li className='p-3'>
+                <ul className={`md:hidden w-full h-screen fixed bg-black top-[138px] text-white 
+                ${toggle ? 'left-[0]' : ' left-[-100%]'}  `}>
+                    <li className='p-5'>
                         HOme
                     </li>
                     <li className='p-3'>
